e260874c0d77992c45de2f22f9883a9832c1d101
felipealencar <felipealencarlopes@gmail.com>
2014-01-25 14:50:17 -0300

    Criação do projeto.

---
+++ filesystem.diagram/src/filesystem/diagram/edit/helpers/FilesystemBaseEditHelper.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ */
+package filesystem.diagram.edit.helpers;
+
+import org.eclipse.gmf.tooling.runtime.edit.helpers.GeneratedEditHelperBase;
+
+/**
+ * @generated
+ */
+public class FilesystemBaseEditHelper extends GeneratedEditHelperBase {
+
+}
---
+++ filesystem.diagram/src/filesystem/diagram/part/FilesystemLinkDescriptor.java
@@ -0,0 +1,30 @@
+/*
+ * 
+ */
+package filesystem.diagram.part;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.gmf.tooling.runtime.update.UpdaterLinkDescriptor;
+
+/**
+ * @generated
+ */
+public class FilesystemLinkDescriptor extends UpdaterLinkDescriptor {
+	/**
+	 * @generated
+	 */
+	public FilesystemLinkDescriptor(EObject source, EObject destination,
+			IElementType elementType, int linkVID) {
+		super(source, destination, elementType, linkVID);
+	}
+
+	/**
+	 * @generated
+	 */
+	public FilesystemLinkDescriptor(EObject source, EObject destination,
+			EObject linkElement, IElementType elementType, int linkVID) {
+		super(source, destination, linkElement, elementType, linkVID);
+	}
+
+}
